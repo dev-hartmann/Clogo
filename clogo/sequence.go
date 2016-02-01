ce2d88738bf3b65e7c5a129cbec2fa1f00dbafd4
Ben Hartmann <ben_hartmann@icloud.com>
2016-02-01 08:21:48 +0100

    additions to ISequence interface

--- clogo/sequence.go
+++ clogo/sequence.go
@@ -1,15 +1,26 @@
 package clogo
 
 type ISequence interface {
-
 	First() interface{}
 	Rest() ISequence
 	Cons() ISequence
+	Concat() ISequence
+
+	Empty() bool
+
+	Take(n int) ISequence
+	Drop(n int) ISequence
+
+	TakeWhile(func(interface{}) bool) ISequence
+	DropWhile(func(interface{}) bool) ISequence
+
+	Sort() ISequence
+	SortBy(predicate interface{}) ISequence
 
 	//Useful functions on sequences
-	Map() ISequence
-	Filter() ISequence
-	Reduce() ISequence
+	Map(func(interface{}) bool) ISequence
+	Filter(func(interface{}) bool) ISequence
+	Reduce(func(interface{}) bool) ISequence
 }
 
 type Sequence struct {
