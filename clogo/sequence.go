0ec717ec0ae1b2a6f05e73b78add244c14394c64
Ben Hartmann <ben_hartmann@icloud.com>
2016-02-01 08:45:15 +0100

    addition to sequence implementation

--- clogo/sequence.go
+++ clogo/sequence.go
@@ -24,4 +24,29 @@ type ISequence interface {
 }
 
 type Sequence struct {
+	head SequenceElement
+
 }
+
+func NewSequence(first SequenceElement) *Sequence {
+	return &Sequence{
+		head: first,
+	}
+}
+
+func(s *Sequence)First() interface{} {
+	return s.head
+}
+
+
+type SequenceElement struct {
+	Value interface{}
+	Next  SequenceElement
+}
+
+func NewSequenceElement(value interface{}, next interface{}) *SequenceElement {
+	return &SequenceElement{
+		Value: value,
+	}
+}
+
